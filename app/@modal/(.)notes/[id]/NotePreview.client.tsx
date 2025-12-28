e352f4fc24f2b2d57da3743f753ab51e9dcd8be7
Yevhiniia Ripa <evgeniarepa@gmail.com>
2025-12-28 04:27:47 +0200

    Update NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from '@tanstack/react-query';
 import { useRouter, useParams } from 'next/navigation';
-import { fetchNoteById } from '@/lib/api';
+import { fetchNoteById } from '@/lib/api/clientApi';
 import Modal from '@/components/Modal/Modal';
 import css from './NotePreview.module.css';
 
@@ -52,5 +52,3 @@ function NotePreview() {
 }
 
 export default NotePreview;
-
-
